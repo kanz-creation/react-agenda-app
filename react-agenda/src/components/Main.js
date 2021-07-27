e779da01d81f561819ad28a6f936efb5344a040c
kanz-creation <kaneez24@my.yorku.ca>
2021-07-26 23:16:12 -0400

    User Can Add Tasks into Array

--- react-agenda/src/components/Main.js
+++ react-agenda/src/components/Main.js
@@ -2,20 +2,31 @@ import React from 'react';
 import TaskList from './TaskList';
 import UserTask from './UserTask';
 
+const tasks = [];
 export default class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      allTasks: [],
+      tasks: tasks,
     };
   }
-
+  //pass task as a parameter
+  userTask = (task) => {
+    if (task.trim() === '') {
+      alert('Task should not be empty');
+      return;
+    }
+    //task added to list, by default, the task is not completed
+    tasks.push({ task, isCompleted: false });
+    //changes reflted in UI
+    this.setState({ tasks: tasks });
+  };
   render() {
     return (
       <div>
         <h1> Today Agenda</h1>
         <div>
-          <UserTask />
+          <UserTask userTask={this.userTask} />
           <br />
           <TaskList />
         </div>
--- react-agenda/src/components/UserTask.js
+++ react-agenda/src/components/UserTask.js
@@ -1,7 +1,40 @@
 import React, { Component } from 'react';
 
 export default class userTask extends Component {
+  // call props cuz = ensure this.propers is set before constructor exists
+  constructor(props) {
+    // cant use this, reference to parent Component, helps to avoid errors
+    super(props);
+    //now its okay
+    this.state = {
+      task: '',
+    };
+  }
+  //this function is for Allowing you to type into the box, handleChange will notice the changes
+  handleChange = (event) => {
+    //retrieves value of whatever input it was called on
+    this.setState({ task: event.target.value });
+  };
+
+  //function allows not to be refreshed by using default value
+  handleSubmit = (event) => {
+    event.preventDefault();
+    //need to pass user tasks to all classes and not just here
+    this.props.userTask(this.state.task);
+    this.setState({ task: '' });
+  };
   render() {
-    return <div>Add Task</div>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="Please enter your task"
+          value={this.state.task}
+          onChange={this.handleChange}
+          autoFocus
+        ></input>
+        <button type="submit">Add Tasks</button>
+      </form>
+    );
   }
 }
